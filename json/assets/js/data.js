a62b1ae31efd9c5aae3471ffed9f2a03b87b0949
TaimoorQadri123 <dchota374@gmail.com>
2024-10-07 22:31:57 +0500

    done

---
+++ json/assets/js/data.js
@@ -0,0 +1,49 @@
+// $(document).ready(function(){
+//     $.ajax({
+//         url:"assets/data.json",
+//         type:"get",
+//         success:function(brands){
+//             let x ="";
+//             $.each(brands,function(keys,arrays){
+//                 $.each(arrays,function(index,objects){
+//                     x+= `   <div class="col-lg-3">
+//                     <div class="card">
+//                         <img class="card-img-top" src="${objects.image}" alt="Title" />
+//                         <div class="card-body">
+//                             <h4 class="card-title">${objects.name}</h4>
+//                             <p class="card-text">${objects.price}</p>
+//                         </div>
+//                     </div>
+                    
+//                 </div>  `
+//                 })
+//             })
+//             $("carddata").html(x)
+//         }
+//     })
+// })
+
+$(document).ready(function(){
+    $.ajax({
+        url: "assets/data.json",
+        type: "get",
+        success: function(brands){ // Corrected from 'sucess' to 'success'
+            let x = "";
+            $.each(brands, function(keys, arrays){
+                $.each(arrays, function(index, objects){
+                    x += `<div class="col-lg-3">
+                            <div class="card mt-5">
+                                <img class="card-img-top" src="${objects.image}" alt="Title" />
+                                <div class="card-body">
+                                    <h4 class="card-title">${objects.name}</h4>
+                                    <p class="card-text">${objects.price}</p>
+                                    <button classs="btn btn-primary">Click here</button>
+                                </div>
+                            </div>
+                        </div>`;
+                });
+            });
+            $("#carddata").html(x); // Also added # to the selector for the correct ID targeting
+        }
+    });
+});
